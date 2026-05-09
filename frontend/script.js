7fb02ec6e61c953609dfaaf5e9756e453c0da30b
sadeen Alkhalili <sadeenkhalili334@gmail.com>
2026-05-09 10:08:46 +0300

    modifying the frontend code,public web

--- frontend/script.js
+++ frontend/script.js
@@ -37,7 +37,7 @@ translateBtn.addEventListener("click", async () => {
   formData.append("file", selectedFile);
 
   try {
-    const response = await fetch("http://127.0.0.1:8000/translate", {
+    const response = await fetch("https://task-2-intern.onrender.com/translate", {
       method: "POST",
       body: formData,
     });
